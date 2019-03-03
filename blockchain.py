63ab1bc0c4579b649d378031bf728b7ccd43c376
Taddes Korris <taddes.korris@gmail.com>
2019-03-02 22:40:11 -0500

    updates

---
+++ blockchain.py
@@ -0,0 +1,13 @@
+blockchain = [1]
+
+def get_last_blockchain_value():
+  return blockchain[-1]
+
+
+def add_value(transaction_amount):
+  blockchain.append(get_last_blockchain_value(),  transaction_amount)
+  print(blockchain)
+
+add_value(2.5)
+add_value(5.6)
+add_value(3.4)
