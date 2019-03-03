fac7f973972c8fd8eb020123c0de3f85a35e8195
Taddes Korris <taddes.korris@gmail.com>
2019-03-03 13:36:44 -0500

    conditionals

--- blockchain.py
+++ blockchain.py
@@ -1,23 +1,56 @@
 blockchain = []
 
 def get_last_blockchain_value():
+  """ Returns the last value of the current blockchain. """
+  if len(blockchain) < 1:
+    return None
   return blockchain[-1]
 
 
 def add_value(transaction_amount, last_transaction=[1]):
+  """ Append a new value, as well as the last blockchain value to the blockchain
+
+  Arguments:
+    :transaction_amount: The amount that should be added.
+    :last_transaction: The last blockchain transation (default[1]).
+  """
+  if last_transaction ==  None:
+    last_transaction = [1]
   blockchain.append([last_transaction,  transaction_amount])
 
-def get_user_input():
-  return float(input('Your transaction amount, please: '))
+def get_transaction_value():
+  """ Returns the input of the user (a new transaction amount) as a float """
+  # Get the user input, transform it from a string to a float and store it
+  user_input = float(input('Your transaction amount, please: '))
+  return user_input
+
+def get_user_choice():
+  user_input = input('Your choice: ')
+  return user_input
 
 
-tx_amount = get_user_input()
+def print_blockchain_elements():
+  for block in blockchain:
+    print('Outputting block:')
+    print (block)
+
+tx_amount = get_transaction_value()
 add_value(tx_amount)
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
+while True:
+    print('Please choose an option:')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('q: Output the blockchain blocks')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+      tx_amount = get_transaction_value()
+      add_value(tx_amount, get_last_blockchain_value())
+    elif user_choice == '2':
+      print_blockchain_elements()
+    elif user_choice == 'q':
+      break
+    else:
+      print('Invalid input')
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
-
-print(blockchain)
+print('Done!')
