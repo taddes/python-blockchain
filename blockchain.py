4e46fe7cdcb47dab398b93dc6b621f275235cc2c
Taddes Korris <taddes.korris@gmail.com>
2019-03-07 14:06:06 -0500

    changes

--- blockchain.py
+++ blockchain.py
@@ -8,6 +8,31 @@ genesis_block =  {
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Taddes'
+participants = {'Taddes'}
+
+
+def hash_block(block):
+    return  '-'.join([str(block[key]) for key in block])
+
+
+def get_balance(participant):
+    """ Find out how much participant has sent (how many transactions they are sender)
+        and to determine how much the participant has recieved (how many transactions they are recipient)
+        :tx_sender: returned variable that gives 
+    """
+    tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
+    amount_sent = 0
+    for tx in tx_sender:
+      if len(tx) > 0:
+          amount_sent += tx[0]
+    tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
+    amount_received = 0
+    for tx in tx_sender:
+      if len(tx) > 0:
+          amount_received += tx[0]
+
+    return amount_received - amount_sent
+
 
 def get_last_blockchain_value():
   """ Returns the last value of the current blockchain. """
@@ -30,6 +55,9 @@ def add_transaction(recipient, sender=owner, amount=1.0):
     'amount': amount
   }
   open_transactions.append(transaction)
+  # Since this is defined as a set, any duplicate vlaues will be ignored!
+  participants.add(sender)
+  participants.add(recipient)
   
 
 def mine_block():
@@ -37,18 +65,17 @@ def mine_block():
       Can invalidate following block if hash does not match previous.
   """  
   last_block = blockchain[-1]
-  hashed_block = '-'.join([str(last_block[key]) for key in last_block])
+  hashed_block = hash_block(last_block)
   # for key in last_block:
   #   value = last_block[key]
     # hashed_block = hashed_block + str(value)
-  print(hashed_block)
-
   block = {
     'previous_hash': hashed_block,
     'index': len(blockchain),
     'transactions': open_transactions
   }
   blockchain.append(block)
+  return True
 
 def get_transaction_value():
   """ Returns the input of the user (a new transaction amount) as a float """
@@ -63,20 +90,15 @@ def get_user_choice():
   return user_input
 
 def verify_chain():
-  # block_index = 0
-  is_valid = True
-  for block_index in range(len(blockchain)):
-    if block_index == 0:
-      continue
-    elif blockchain[block_index] [0] == blockchain[block_index - 1]:
-      is_valid = True
-    else:
-      is_valid = False
-      # break
-  return is_valid
+    """ Verify the current blockchain and return True if it's valid, False if not """
+    for (index, block) in enumerate(blockchain):
+        if index == 0:
+            continue
+        if block['previous_hash'] != hash_block(blockchain[index - 1]):
+            return False
+    return True
 
 waiting_for_input = True
-
 
 def print_blockchain_elements():
   for block in blockchain:
@@ -93,6 +115,7 @@ while waiting_for_input:
     print('1: Add a new transaction value')
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
+    print('4: Output the participants')
     print('h: Manipulate the chain')
     print('q: Output the blockchain blocks')
     user_choice = get_user_choice()
@@ -103,20 +126,28 @@ while waiting_for_input:
       add_transaction(recipient, amount=amount)
       print(open_transactions)
     elif user_choice == '2':
-      mine_block()
+      if mine_block():
+          open_transactions = []
     elif user_choice == '3':
       print_blockchain_elements()
+    elif user_choice == '4':
+      print(participants)
     elif user_choice == 'h':
       if len(blockchain) >= 1:
-        blockchain[0] = [2]
+        blockchain[0] =  {
+          'previous_hash': '',
+          'index': 0,
+          'transactions': [{'sender': 'Chris', 'recipient': 'Max', 'amount': 100 }]
+        }
     elif user_choice == 'q':
       waiting_for_input = False
     else:
-      print('Invalid input')
-    # if not verify_chain():
-    #  print_blockchain_elements()
-    #  print('Invalid blockchain!')
-    #  break
+        print('Invalid input')
+    if not verify_chain():
+        print_blockchain_elements()
+        print('Invalid blockchain!')
+        break
+    print(get_balance('Taddes'))
 else:
   print('User left')
 
--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-blockchain
+# List Comprehension
+blockchain = [5, 10, 15, 20]
+blockchain_double = []
+
+for key in blockchain: 
+    blockchain_double.append(key * 2)
+
+print(blockchain_double)
+
+# Comprehension Example
+print('Enumeration Example')
+blockchain_double = [key * 2 for key in blockchain]
+
+print(blockchain_double)
